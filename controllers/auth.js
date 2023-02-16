3837c36c0a7dbe32eb7cb88a016fd42091a8ba51
Hardik Sakpal <hardik.sakpal@synverse.com>
2023-02-17 01:00:17 +0530

    vehicle no updated 1

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,7 @@ const userSchema = Joi.object().keys({
   email: Joi.string().email({ minDomainSegments: 2 }),
   userType: Joi.string().required(),
   password: Joi.string().required().min(4),
-  vehicleNo: Jio.string(),
+  vehicleNo: Joi.string(),
   confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
 });
 
