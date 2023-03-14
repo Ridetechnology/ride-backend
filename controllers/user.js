bc8a126437e7714e59a0d79dc9e90bba7d5a227a
Hardik Sakpal <hardik.sakpal@synverse.com>
2023-03-14 19:59:09 +0530

    update user api

--- controllers/user.js
+++ controllers/user.js
@@ -66,11 +66,29 @@ exports.updateRides = async (req, res) => {
     return res.status(200).json({
       message: "Rides updated successfully",
       passenger: passenger,
-      driver: driver
+      driver: driver,
     });
   } catch (error) {
     res.status(500).json({
       message: "Cannot Update Rides",
+      error: error.message,
+    });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const userData = await user.findByIdAndUpdate(id, req.body, { new: true });
+    if (!userData) return res.status(404).send("User was not updated");
+    return res.status(200).json({
+      message: "User updated successfully",
+      user: userData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Cannot Update User",
       error: error.message,
     });
   }
--- routers/user.js
+++ routers/user.js
@@ -6,5 +6,6 @@ const auth = require("../middleware/authentication");
 router.put("/updateRides", userController.updateRides);
 router.get("/getDriversByRoute", userController.getDriversByRoute);
 router.put("/updateDriverRoute/:id", userController.updateDriverRoute);
+router.put("/updateUser/:id", userController.updateUser);
 
 module.exports = router;
